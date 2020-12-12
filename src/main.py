db62d4cfb4da6287237bdb9b15ecba444232c8ed
Falguni Das <dasshuvo7@gmail.com>
2020-12-12 21:20:09 +0600

    Refactor main.py.

--- src/main.py
+++ src/main.py
@@ -1,13 +1,13 @@
 from saved_models import saved_model_dir
 from src.environment.impl.cartpole_environment import CartPoleEnvironment
-from src.policy.reinforce import Reinforce
+from src.policy.reinforce_with_baseline import ReinforceWithBaseline
 from src.utils.summary_writer import SummaryWriter
 
 environment = CartPoleEnvironment()
 summary_writer = SummaryWriter()
 save_model_path = saved_model_dir
 
-agent_policy = Reinforce(environment, summary_writer, save_model_path=save_model_path)
+agent_policy = ReinforceWithBaseline(environment, summary_writer, save_model_path=save_model_path)
 
 agent_policy.learn_optimal_policy()
 
